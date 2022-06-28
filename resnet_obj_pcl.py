d2506f1d358dffc3c6d7027465991986d0fb3617
Nuri Kim <nuri.kim@rllab.snu.ac.kr>
2022-06-28 20:32:55 +0900

    add obj pcl

--- resnet_obj_pcl.py
+++ resnet_obj_pcl.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+# from torchvision.models.utils import load_state_dict_from_url
+
+try:
+    from torch.hub import load_state_dict_from_url  # noqa: 401
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url  # noqa: 401
 from torchvision.ops import roi_align
 
 
@@ -160,8 +165,7 @@ class ResNet(nn.Module):
         self.object_compression = nn.Sequential(
             nn.Linear(128 * 7 * 7, 32 * 2),
             nn.ReLU(),
-            nn.Linear(32 * 2, 32),
-            nn.LayerNorm(32),
+            nn.Linear(32 * 2, 32)
         )
 
         for m in self.modules():
