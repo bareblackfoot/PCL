a984b65b5b1033f53f804c85808b3a92e8800d20
Nuri Kim <nuri.kim@rllab.snu.ac.kr>
2022-07-31 12:18:32 +0900

    add video

--- main_pcl_scene_agno.py
+++ main_pcl_scene_agno.py
@@ -110,6 +110,7 @@ parser.add_argument(
     help="include stop action or not",
 )
 
+
 def main():
     args = parser.parse_args()
 
@@ -422,7 +423,7 @@ def compute_features(eval_loader, model, args):
     print('Computing features...')
     model.eval()
     features = torch.zeros(len(eval_loader.dataset),args.low_dim).cuda()
-    for i, (images, index) in enumerate(tqdm(eval_loader)):
+    for i, (images, index, _) in enumerate(tqdm(eval_loader)):
         with torch.no_grad():
             images = images.cuda(non_blocking=True)
             feat = model(images,is_eval=True) 
