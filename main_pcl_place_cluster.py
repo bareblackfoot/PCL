3b144b510ebeccc17aeaa15eeccd6ef7639bdc21
Nuri Kim <nuri.kim@rllab.snu.ac.kr>
2022-10-07 12:04:57 +0900

    edit object gcn

--- main_pcl_place_cluster.py
+++ main_pcl_place_cluster.py
@@ -269,17 +269,9 @@ def main_worker(gpu, ngpus_per_node, args):
             if np.any([torch.any(torch.isnan(cluster_result['density'][i])).item() for i in range(len(cluster_result['density']))]):
                 cluster_result = None
                 print('NaN detected in density, skip this epoch')
-                idx = np.where(np.isinf(features.sum(-1)))[0]
-                idx = idx + np.where(np.isnan(features.sum(-1)))[0]
-                print(idx)
-                print([train_dataset.data_list[id_] for id_ in idx])
             elif np.any([torch.any(torch.isinf(cluster_result['density'][i])).item() for i in range(len(cluster_result['density']))]):
                 cluster_result = None
                 print('Inf detected in density, skip this epoch')
-                idx = np.where(np.isinf(features.sum(-1)))[0]
-                idx = idx + np.where(np.isnan(features.sum(-1)))[0]
-                print(idx)
-                print([train_dataset.data_list[id_] for id_ in idx])
 
         if args.distributed:
             train_sampler.set_epoch(epoch)
